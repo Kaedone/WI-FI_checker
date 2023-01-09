6f2cc97a258ddedb597246a5b5bc592e6a3b2b52
Kaedone <timstab1@gmail.com>
2023-01-09 23:05:05 +0300

    add automatic system lang revial"

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import get_wifi as wifi
 
+import os
+import ctypes
+import locale
+
 def main():
-    wifi_list = wifi.get_wifi_win('ru') # TODO: add language parameter
+    vers=os.name
+    if vers=="nt":
+        win_lang=ctypes.windll.kernel32
+        win_lang=locale.windows_locale[win_lang.GetUserDefaultUILanguage()]
+
+        if win_lang=="ru_RU":
+
+            wifi_list=wifi.get_wifi_win("ru")
+        else:
+            wifi_list=wifi.get_wifi_win("en")
+        print(wifi_list)
+    if vers=="posix":
+        wifi_list=wifi.get_wifi_linux()
+        print(wifi_list)
+    
     
