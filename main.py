b0bd83d8e653774e0f8a043e26b7893ff6281c52
Kaedone <timstab1@gmail.com>
2023-01-09 22:55:06 +0300

    add linux vers

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import subprocess # import subprocess module
+import get_wifi as wifi
 
-wi_fi_list=subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('CP866', errors='ignore').split('\n')# get the list of all the wifi profiles
-# if you system languade is not Russian - use UTF-8
-
-wi_fi_names=[]
-
-for wi_fi in wi_fi_list:
-    if "Все профили пользователей" in wi_fi:
-        wi_fi_names.append(wi_fi.split(":")[1][1:-1]) # get the name of the wifi profile
-
-print(wi_fi_names)
-
+def main():
+    wifi_list = wifi.get_wifi_win('ru') # TODO: add language parameter
+    
